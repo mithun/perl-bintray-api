9f709114be0189752cf4aee0cd6536a3f0f75111
Mithun Ayachit <m0t0rbr3th@gmail.com>
2013-10-01 06:47:40 -0500

    initial documentation

--- .author/pod_helper.pl
+++ .author/pod_helper.pl
@@ -35,3 +35,143 @@ $version->upload(
     publish   => 1,
 );
 
+# Initialize a subject
+my $subject = $btray->subject(
+
+    # Provide an optional _name_
+    #	- defaults to the current username
+    name => 'my_company',
+);
+
+# Subject methods
+my $info       = $subject->info();          # Get info
+my $followers  = $subject->followers();     # Get followers
+my $hooks      = $subject->get_webhooks();  # Get Registered hooks
+my $repos      = $subject->repos();         # Get user repositories
+my @repo_names = $subject->repo_names();    # Shortcut to list repo names
+
+# Initialize a Repo
+my $repo = $subject->repo( name => 'myrepo' );
+
+# Repository methods
+my $info          = $repo->info();           # Repo information
+my $packages      = $repo->packages();       # Get packages
+my @package_names = $repo->package_names();  # List of packages
+my $hooks         = $repo->get_webhooks();   # Hooks listing
+
+## Repository Operations
+
+# Create Package
+$repo->create_package(
+    details => {
+        name     => 'MyPackage',
+        desc     => 'foo bar',
+        labels   => [qw(foo)],
+        licenses => [ 'GPL-3.0', 'Artistic-License-2.0', ],
+    },
+);
+
+# Delete Package
+$repo->delete_package( name => 'MyPackage' );
+
+## Initialize Package
+my $package = $repo->package( name => 'MyPackage' );
+my $info = $package->info();
+
+## Package operations
+
+# Update
+$package->update( details => {...} );
+
+# Versions
+$package->create_version(
+    details => {
+        name          => '1.0',
+        release_notes => 'foobar',
+        release_url   => 'http://foo/bar'
+    }
+);
+$package->delete_version( name => '1.0' );
+
+# Attributes
+$package->get_attributes();
+$package->set_attributes(
+    attributes => [
+        {
+            name   => 'foo',
+            values => [qw(bar baz)],
+            type   => 'string',
+        },
+    ],
+);
+$package->update_attributes( attributes => [...] );
+
+# Hooks
+$package->set_webhook( url => 'http://....' );
+$package->delete_webhook();
+
+## Initialize Versions
+my $version = $package->version( name => '1.0' );
+my $info = $version->info();  # Info
+
+## Version Operations
+
+# Upload
+$version->upload(
+    file      => '/path/to/local/file',
+    repo_path => 'myfiles/file',
+
+    # Optional params
+    publish => 0,  # Publish on upload
+    explode => 0,  # Upload an exploded archive
+);
+
+# Update details
+$version->update(
+    details => {
+        desc   => 'version description',
+        labels => [qw(foo bar)],
+    },
+);
+
+# Sign
+$version->sign( passphrase => '...' );
+
+# Publish
+$version->publish();
+
+# Discard
+$version->discard();
+
+# Attributes
+my $attributes = $version->get_aatributes();
+$version->set_attributes( attributes => [...] );
+$version->update_attributes( attributes => [...] );
+
+## Init Search object
+my $search = $btray->search();
+
+# Search Repos
+@results = $search->repos(
+    name => 'myrepo',     # // either name or desc is required
+    desc => 'maven repo',
+);
+
+# Search Packages
+@results = $search->packages(
+    name    => '...',
+    desc    => '...',
+    repo    => '...',
+    subject => '...',     # // User or Org
+);
+
+# Search files
+@results = $search->files(
+    name => '...',
+    sha1 => '...',
+    repo => '...',
+);
+
+# Search Users or Organizations
+@results = $search->users( name => '...' );
+
--- lib/Bintray/API.pm
+++ lib/Bintray/API.pm
@@ -108,9 +108,159 @@ This module provides a Perl wrapper to the L<Bintray REST API|https://bintray.co
 
 =head1 API Methods
 
-This module is structured similar to L<Bintray Entities|https://bintray.com/docs/bintrayuserguide.html#_bintray_entities>.
+This distribution is structured similar to L<Bintray Entities|https://bintray.com/docs/bintrayuserguide.html#_bintray_entities>.
 
-=head1 DEPENDENCIES
+=head2 SUBJECT
+
+A I<subject> is either a user or an organization
+
+	# Initialize a subject
+	my $subject = $btray->subject(
+
+	    # Provide an optional _name_
+	    #	- defaults to the current username
+	    name => 'my_company',
+	);
+
+	# Subject methods
+	my $info       = $subject->info();          # Get info
+	my $followers  = $subject->followers();     # Get followers
+	my $hooks      = $subject->get_webhooks();  # Get Registered hooks
+	my $repos      = $subject->repos();         # Get user repositories
+	my @repo_names = $subject->repo_names();    # Shortcut to list repo names
+
+=head2 REPOSITORY
+
+	# Initialize a Repo
+	my $repo = $subject->repo( name => 'myrepo' );
+
+	# Repository methods
+	my $info          = $repo->info();           # Repo information
+	my $packages      = $repo->packages();       # Get packages
+	my @package_names = $repo->package_names();  # List of packages
+	my $hooks         = $repo->get_webhooks();   # Hooks listing
+
+	## Repository Operations
+
+	# Create Package
+	$repo->create_package(
+	    {
+	        name     => 'MyPackage',
+	        desc     => 'foo bar',
+	        labels   => [qw(foo)],
+	        licenses => [ 'GPL-3.0', 'Artistic-License-2.0', ],
+	    }
+	);
+
+	# Delete Package
+	$repo->delete_package( name => 'MyPackage' );
+
+=head2 PACKAGE
+
+	## Initialize Package
+	my $package = $repo->package( name => 'MyPackage' );
+	my $info = $package->info();
+
+	## Package operations
+
+	# Update
+	$package->update( details => {...} );
+
+	# Versions
+	$package->create_version(
+	    details => {
+	        name          => '1.0',
+	        release_notes => 'foobar',
+	        release_url   => 'http://foo/bar'
+	    }
+	);
+	$package->delete_version( name => '1.0' );
+
+	# Attributes
+	$package->get_attributes();
+	$package->set_attributes(
+	    attributes => [
+	        {
+	            name   => 'foo',
+	            values => [qw(bar baz)],
+	            type   => 'string',
+	        },
+	    ],
+	);
+	$package->update_attributes( attributes => [...] );
+
+	# Hooks
+	$package->set_webhook( url => 'http://....' );
+	$package->delete_webhook();
+
+=head2 VERSIONS
+
+	## Initialize Versions
+	my $version = $package->version( name => '1.0' );
+	my $info = $version->info();  # Info
+
+	## Version Operations
+
+	# Upload
+	$version->upload(
+	    file      => '/path/to/local/file',
+	    repo_path => 'myfiles/file',
+
+	    # Optional params
+	    publish => 0,  # Publish on upload
+	    explode => 0,  # Upload an exploded archive
+	);
+
+	# Update details
+	$version->update(
+	    details => {
+	        desc   => 'version description',
+	        labels => [qw(foo bar)],
+	    },
+	);
+
+	# Sign
+	$version->sign( passphrase => '...' );
+
+	# Publish
+	$version->publish();
+
+	# Discard
+	$version->discard();
+
+	# Attributes
+	my $attributes = $version->get_aatributes();
+	$version->set_attributes( attributes => [...] );
+	$version->update_attributes( attributes => [...] );
+
+=head2 SEARCH
+
+	## Init Search object
+	my $search = $btray->search();
+
+	# Search Repos
+	@results = $search->repos(
+	    name => 'myrepo',     # // either name or desc is required
+	    desc => 'maven repo',
+	);
+
+	# Search Packages
+	@results = $search->packages(
+	    name    => '...',
+	    desc    => '...',
+	    repo    => '...',
+	    subject => '...',     # // User or Org
+	);
+
+	# Search files
+	@results = $search->files(
+	    name => '...',
+	    sha1 => '...',
+	    repo => '...',
+	);
+
+	# Search Users or Organizations
+	@results = $search->users( name => '...' );
 
 =head1 BUGS AND LIMITATIONS
 
